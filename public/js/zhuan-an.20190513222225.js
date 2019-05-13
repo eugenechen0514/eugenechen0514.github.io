7d84b472878830f98ae1e1c8337b4f60e2815132
EugeneChen <yujiechen0514@gmail.com>
2019-05-13 22:22:42 +0800

    fix layout

--- public/js/zhuan-an.20190513222225.js
+++ public/js/zhuan-an.20190513222225.js
@@ -3,20 +3,20 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 
 $(document).ready(function() {
-r=function(){dpi=window.devicePixelRatio;$('.js22').attr('src', (dpi>1) ? 'images/you-turn-1018.jpg' : 'images/you-turn-509.jpg');
+r=function(){dpi=window.devicePixelRatio;$('.js22').attr('src', (dpi>1) ? 'images/dockpad-972.jpg' : 'images/dockpad-486.jpg');
 $('.js23').attr('src', (dpi>1) ? 'images/progressive-mesh-1018.jpg' : 'images/progressive-mesh-509.jpg');
 $('.js24').attr('src', (dpi>1) ? 'images/tadis-1018-1.jpg' : 'images/tadis-509-1.jpg');
-$('.js15').attr('src', (dpi>1) ? 'images/qrcode1-450.jpg' : 'images/qrcode1-225.jpg');
-$('.js16').attr('src', (dpi>1) ? 'images/qrcode2-692.png' : 'images/qrcode2-346.png');
-$('.js17').attr('src', (dpi>1) ? 'images/qrcode3-452.jpg' : 'images/qrcode3-226.jpg');
-$('.js20').attr('src', (dpi>1) ? 'images/dockpad-972.jpg' : 'images/dockpad-486.jpg');};
+$('.js16').attr('src', (dpi>1) ? 'images/qrcode1-446.jpg' : 'images/qrcode1-223.jpg');
+$('.js17').attr('src', (dpi>1) ? 'images/qrcode2-692.png' : 'images/qrcode2-346.png');
+$('.js18').attr('src', (dpi>1) ? 'images/qrcode3-448.jpg' : 'images/qrcode3-224.jpg');
+$('.js21').attr('src', (dpi>1) ? 'images/you-turn-1018.jpg' : 'images/you-turn-509.jpg');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 var consent = new ConsentBanner('privacy-policy.html', 1);var wl = new woolite();
 wl.init();
-wl.addAnimation($('.js18')[0], "3.20s", "0.00s", 3, 100);
-wl.addAnimation($('.js19')[0], "1.00s", "0.00s", 1, 100);
-wl.addAnimation($('.js21')[0], "3.20s", "0.00s", 3, 100);
+wl.addAnimation($('.js15')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js19')[0], "3.20s", "0.00s", 3, 100);
+wl.addAnimation($('.js20')[0], "3.20s", "0.00s", 3, 100);
 wl.addAnimation($('.js25')[0], "2.00s", "0.00s", 1, 100);
 wl.start();
 
